c3490d245491b1803963db00daf31db31e106007
Wahah479 <132962725+Wahah479@users.noreply.github.com>
2023-05-13 14:23:17 -0400

    Add playRound function to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 function getComputerChoice() {
     let ranNum = Math.floor(Math.random() * 3);
+    console.log(ranNum);
     if (ranNum == 0) {
         return "Rock";
     }
@@ -12,7 +13,42 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    let player = playerSelection.toLowerCase();
+    let com = computerSelection.toLowerCase();
+    if (player === com){
+        return `You Tie! Both chose ${computerSelection}`;
+    }
+    //No Tie  | User chose Rock
+    else if (player === "rock") {
+        if (com === "paper") {
+            return "You Lose! Paper beats Rock";
+        }
+        else {
+            return "You Win! Rock beats Scissors";
+        }
+    }
+    else if (player === "paper") {
+        if (com === "rock") {
+            return "You Win! Paper beats Rock";
+        }
+        else {
+            return "You Lose! Scissors beat Paper";
+        }
+    }
+    else {
+        if (com === "rock") {
+            return "You Lose! Rock beats Scissors";
+        }
+        else {
+            return "You Win! Scissors beat Paper";
+        }
+    }
 }
 
-console.log(getComputerChoice());
+function game() {
+    for (let i = 0; i < 5; i++) {
+        
+    }
+}
+
+console.log(playRound("Scissors", getComputerChoice()));
